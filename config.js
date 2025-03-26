f54bfd8d538b884cf187d64ed10fb3ab2611027e
Eskndr <eskndrytbarek323@gmail.com>
2025-03-27 02:48:12 +0300

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDatabase = async () => {
   try {
     const connection = await mongoose.connect(
-      "mongodb+srv://eskndr:0N4O4xr2znWMgxFE@cluster0.eefsa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+      "mongodb+srv://eskndr:0N4O4xr2znWMgxFE@cluster0.eefsa.mongodb.net/work-out?retryWrites=true&w=majority&appName=Cluster0"
     );
     console.log("Database Connected");
   } catch (error) {
